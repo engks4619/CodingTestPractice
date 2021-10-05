e6d075589c93aa34b1668d65b01cbd44dcdcf54a
engks4619 <engks4619@naver.com>
2021-10-05 23:07:51 +0900

    2021-10-05

--- programmers/Level2/쿼드압축 후 개수 세기.py
+++ programmers/Level2/쿼드압축 후 개수 세기.py
@@ -1,3 +1,4 @@
+'''
 def solution(arr):
     def divide(x, y, n):
         if n == 1:
@@ -12,5 +13,25 @@ def solution(arr):
         else:
             return list(map(sum,zip(lu,ru,ld,rd)))
     answer = divide(0, 0, len(arr))
+    return answer
+'''
+def solution(arr):
+    answer = [0,0]
+    def divide(y, x, n):
+        if n == 1:
+            answer[arr[y][x]] += 1
+            return
+        else:
+            prev = arr[y][x]
+            for dy in range(n):
+                for dx in range(n):
+                    if prev != arr[y+dy][x+dx]:
+                        divide(y, x, n//2)
+                        divide(y, x + n//2, n//2)
+                        divide(y + n//2, x, n//2)
+                        divide(y + n//2, x + n//2, n//2)
+                        return
+            answer[prev] += 1
+    divide(0, 0, len(arr))
     return answer
 solution([[1,1,0,0],[1,0,0,0],[1,0,0,1],[1,1,1,1]])
