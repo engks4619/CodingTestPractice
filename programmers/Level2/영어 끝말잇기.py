f957bae4398d4b9b7606a34e1dc16dc27d5161b0
engks4619 <engks4619@naver.com>
2021-10-09 16:34:27 +0900

    2021-10-09

--- programmers/Level2/영어 끝말잇기.py
+++ programmers/Level2/영어 끝말잇기.py
@@ -1,13 +1,10 @@
 def solution(n, words):
     word_lst = []
     for idx, word in enumerate(words):
-        if word in word_lst:
+        if word in word_lst or (word_lst and word_lst[-1][-1] != word[0]):
             return [idx % n + 1, idx // n + 1]     
         else:
-            if word_lst and word_lst[-1][-1] != word[0]:
-                return [idx % n + 1, idx // n + 1]   
-            else:
-                word_lst.append(word)
+            word_lst.append(word)
     return [0,0]
 
 solution(3,["tank", "kick", "know", "wheel", "land", "dream", "mother", "robot", "tank"])
