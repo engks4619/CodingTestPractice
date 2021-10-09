5436de3f926fb766b8aad30f60f1850b1811f770
engks4619 <engks4619@naver.com>
2021-10-09 16:28:48 +0900

    2021-10-09

---
+++ programmers/Level2/영어 끝말잇기.py
@@ -0,0 +1,9 @@
+def solution(n, words):
+    answer = []
+    dic = {i: 0 for i in range(1,n+1)}
+    print(dic)
+    for word in words:
+        pass
+    return answer
+
+solution(3,["tank", "kick", "know", "wheel", "land", "dream", "mother", "robot", "tank"])
