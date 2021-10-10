653aad0ff9df8b16e726e7c4ba19f3166398a103
engks4619 <engks4619@naver.com>
2021-10-10 11:27:27 +0900

    2021-10-10

---
+++ programmers/Level2/2개 이하로 다른 비트.py
@@ -0,0 +1,16 @@
+def solution(numbers):
+    answer = []
+    for number in numbers:
+        if number % 2 == 0:
+            answer.append(number+1)
+        else:            
+            bit_number = list("0" + bin(number)[2:])
+            for i in range(len(bit_number)-1,-1):
+                if bit_number[i] == "0":
+                    bit_number[i] = "1"
+                    bit_number[i+1] = "0"
+                    break
+            answer.append(int("".join(bit_number),2))
+    print(answer)
+    return answer
+solution([2,7])
