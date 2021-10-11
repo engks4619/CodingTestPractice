9e94a2f06eb3beb71ec23e62daf3585cadbf2e87
engks4619 <engks4619@naver.com>
2021-10-12 06:05:23 +0900

    2021-10-12

--- programmers/Level2/배달.py
+++ programmers/Level2/배달.py
@@ -1,27 +1,25 @@
 import heapq
 def solution(N, road, K):
-    answer = 0
     INF = int(1e9)
+    distance = [INF for _ in range(N+1)]
     graph = [[] for _ in range(N+1)]
-    d = [INF for _ in range(N+1)]
-    for v in road:
-        graph[v[0]].append((v[1],v[2]))
-        graph[v[1]].append((v[0],v[2]))
-    def dijkstra(start):
-        q = []
-        d[start] = 0
-        heapq.heappush(q,(0,start))
-        while q:
-            dist, now = heapq.heappop(q)
-            if d[now] < dist:
-                continue
-            for i in graph[now]:
-                cost = dist + i[1]
-                if cost < d[i[0]]:
-                    d[i[0]] = cost
-                    heapq.heappush(q, (cost,i[0]))
-    dijkstra(1)
-    answer = len([i for i in d if i <= K])
-    return answer
+    for i in road:
+        start, end, d = i[0], i[1], i[2]
+        graph[start].append((end,d))
+        graph[end].append((start,d))
+    q = []
+    heapq.heappush(q,(0,1))
+    distance[1] = 0
+    while q:
+        dist, now = heapq.heappop(q)
+        if distance[now] < dist:
+            continue
+        for i in graph[now]:
+            cost = distance[now] + i[1]
+            if cost < distance[i[0]]:
+                distance[i[0]] = cost
+                heapq.heappush(q,(cost, i[0]))
+    return len([d for d in distance if d <= K])
+    
 solution(5, [[1,2,1],[2,3,3],[5,2,2],[1,4,2],[5,3,1],[5,4,2]],3)
 solution(6, [[1,2,1],[1,3,2],[2,3,2],[3,4,3],[3,5,2],[3,5,3],[5,6,1]], 4)
