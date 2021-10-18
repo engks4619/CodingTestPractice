0ecccf36125754275edd65736a9a8fde6b5339ac
engks4619 <engks4619@naver.com>
2021-10-19 00:12:23 +0900

    2021-10-19

--- 유형별 기출문제/구현_외벽점검.py
+++ 유형별 기출문제/구현_외벽점검.py
@@ -1,14 +1,19 @@
 from itertools import permutations
-def solution(n, weak, dist):    
-    INF = 1e9
-    answer = INF
+def solution(n, weak, dist): 
+    answer = 1e9
     length = len(weak)
-    weak += [w + n for w in weak]
-    dist.sort(reverse=True)
-    for p in list(permutations(dist)):
-        print(p)
-
-    print(weak)
-    return answer
+    new_weak = weak + [w + n for w in weak]
+    for i in range(length):
+        for dists in list(permutations(dist)):
+            count = 0
+            pos = weak[i]
+            for d in dists:
+                count += 1
+                pos += d
+                if pos < new_weak[i + length - 1]:
+                    pos = [w for w in new_weak if w > pos][0]
+                else:
+                    answer = min(answer, count)
+    return answer if answer != 1e9 else -1
 
 solution(12, [1, 3, 4, 9, 10], [3, 5, 7])
