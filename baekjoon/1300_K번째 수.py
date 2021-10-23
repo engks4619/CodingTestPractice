4817b2b022d72db166eccd609b054af8fa17fa93
engks4619 <engks4619@naver.com>
2021-10-24 08:00:48 +0900

    2021-10-24

---
+++ baekjoon/1300_K번째 수.py
@@ -0,0 +1,13 @@
+n = int(input())
+k = int(input())
+start, end = 1, n * n
+while (start <= end):
+    mid = (start + end) // 2
+    count = 0
+    for i in range(1, n+1):
+        count += min(mid//i, n)
+    if count >= k:
+        end = mid - 1
+    elif count < k:
+        start = mid + 1
+print(start)
