a26b3aa6cebed103759dff4c12b700ac66b7efa7
engks4619 <engks4619@naver.com>
2021-11-09 11:47:11 +0900

    2021-11-09

--- leetcode/연결리스트_2. Add Two Numbers.py
+++ leetcode/연결리스트_2. Add Two Numbers.py
@@ -1,3 +1,5 @@
+'''
+#자료형 변환
 from typing import List
 
 class ListNode:
@@ -33,3 +35,26 @@ class Solution:
 
         result = int(''.join(str(e) for e in a)) + int(''.join(str(e) for e in b))
         return self.toReversedLinkedList(str(result))
+'''
+#전가산기 구현
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+class Solution:
+    def addTwoNumbers(self, l1: ListNode, l2: ListNode) -> ListNode:
+        root = head = ListNode(0)
+        carry = 0
+        while l1 or l2 or carry:
+            sum = 0
+            if l1:
+                sum += l1.val
+                l1 = l1.next
+            if l2:
+                sum += l2.val
+                l2 = l2.next
+            carry, val = divmod(sum+carry, 10)
+            head.next = ListNode(val)
+            head = head.next
+            
+        return root.next
