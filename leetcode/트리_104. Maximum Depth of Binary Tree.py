d51988d6c04aa8221ec229dfc6c3004d19d21741
engks4619 <engks4619@naver.com>
2021-11-22 05:41:47 +0900

    2021-11-22

---
+++ leetcode/트리_104. Maximum Depth of Binary Tree.py
@@ -0,0 +1,21 @@
+from collections import deque
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+class Solution:
+    def maxDepth(self, root: TreeNode) -> int:
+        queue = deque([root])
+        depth = 0
+        while queue:
+            depth += 1
+            for _ in range(len(queue)):
+                cur_root = queue.popleft()
+                if cur_root.left:
+                    queue.append(cur_root.left)
+                if cur_root.right:
+                    queue.append(cur_root.right)
+        return depth
